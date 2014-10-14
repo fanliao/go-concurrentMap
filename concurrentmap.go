d527b0aefea6f85502c5b61d0e33eaaffe3fff52
fanliao <liaofan1973@gmail.com>
2014-10-14 14:53:36 +0800

    Update concurrentmap.go

--- concurrentmap.go
+++ concurrentmap.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"errors"
+	"math"
+	"sync"
+	"sync/atomic"
+)
+
 const (
 	/**
 	 * The default initial capacity for this table,
@@ -33,7 +40,7 @@ const (
 	 */
 	MAX_SEGMENTS int = 1 << 16 // slightly conservative
 
-	/** 
+	/**
 	 * Number of unsynchronized retries in size and containsValue
 	 * methods before resorting to locking. This is used to avoid
 	 * unbounded retries if tables undergo continuous modification
@@ -70,7 +77,344 @@ type ConcurrentMap struct {
  * @return the segment
  */
 func (this *ConcurrentMap) segmentFor(hash uint32) *Segment {
+	//默认segmentShift是28，segmentMask是15（0x1111）,所以hash>>this.segmentShift就是取前面4位
+	//为何还要&uint32(this.segmentMask)？java中>>>是无符号移位，除了前面4位外应该都是0
 	return this.segments[(hash>>this.segmentShift)&uint32(this.segmentMask)]
+}
+
+/**
+ * Creates a new, empty map with the specified initial
+ * capacity, load factor and concurrency level.
+ *
+ * @param initialCapacity the initial capacity. The implementation
+ * performs internal sizing to accommodate this many elements.
+ * @param loadFactor  the load factor threshold, used to control resizing.
+ * Resizing may be performed when the average number of elements per
+ * bin exceeds this threshold.
+ * @param concurrencyLevel the estimated number of concurrently
+ * updating threads. The implementation performs internal sizing
+ * to try to accommodate this many threads.
+ * @throws IllegalArgumentException if the initial capacity is
+ * negative or the load factor or concurrencyLevel are
+ * nonpositive.
+ */
+func NewConcurrentMap3(initialCapacity int,
+	loadFactor float32, concurrencyLevel int) (m *ConcurrentMap) {
+	m = &ConcurrentMap{}
+
+	if !(loadFactor > 0) || initialCapacity < 0 || concurrencyLevel <= 0 {
+		panic(errors.New("IllegalArgumentException"))
+	}
+
+	if concurrencyLevel > MAX_SEGMENTS {
+		concurrencyLevel = MAX_SEGMENTS
+	}
+
+	// Find power-of-two sizes best matching arguments
+	sshift := 0
+	ssize := 1
+	for ssize < concurrencyLevel {
+		sshift++
+		ssize = ssize << 1
+	}
+
+	m.segmentShift = uint(32) - uint(sshift)
+	m.segmentMask = ssize - 1
+
+	m.segments = make([]*Segment, ssize)
+
+	if initialCapacity > MAXIMUM_CAPACITY {
+		initialCapacity = MAXIMUM_CAPACITY
+	}
+
+	c := initialCapacity / ssize
+	if c*ssize < initialCapacity {
+		c++
+	}
+	cap := 1
+	for cap < c {
+		cap <<= 1
+	}
+
+	for i := 0; i < len(m.segments); i++ {
+		m.segments[i] = newSegment(cap, loadFactor)
+	}
+	return
+}
+
+/**
+ * Creates a new map with the same mappings as the given map.
+ * The map is created with a capacity of 1.5 times the number
+ * of mappings in the given map or 16 (whichever is greater),
+ * and a default load factor (0.75) and concurrencyLevel (16).
+ *
+ * @param m the map
+ */
+func NewConcurrentMapFromMap(m map[interface{}]interface{}) *ConcurrentMap {
+	cm := NewConcurrentMap3(int(math.Max(float64(float32(len(m))/DEFAULT_LOAD_FACTOR+1),
+		float64(DEFAULT_INITIAL_CAPACITY))),
+		DEFAULT_LOAD_FACTOR, DEFAULT_CONCURRENCY_LEVEL)
+	cm.PutAll(m)
+	return cm
+}
+
+/**
+ * Returns <tt>true</tt> if this map contains no key-value mappings.
+ *
+ * @return <tt>true</tt> if this map contains no key-value mappings
+ */
+func (this *ConcurrentMap) IsEmpty() bool {
+	segments := this.segments
+	/*
+	 * We keep track of per-segment modCounts to avoid ABA
+	 * problems in which an element in one segment was added and
+	 * in another removed during traversal, in which case the
+	 * table was never actually empty at any point. Note the
+	 * similar use of modCounts in the size() and containsValue()
+	 * methods, which are the only other methods also susceptible
+	 * to ABA problems.
+	 */
+	mc := make([]int32, len(segments))
+	var mcsum int32 = 0
+	for i := 0; i < len(segments); i++ {
+		if atomic.LoadInt32(&segments[i].count) != 0 {
+			return false
+		} else {
+			mc[i] = atomic.LoadInt32(&segments[i].modCount)
+			mcsum += mc[i]
+		}
+	}
+
+	// If mcsum happens to be zero, then we know we got a snapshot
+	// before any modifications at all were made.  This is
+	// probably common enough to bother tracking.
+	if mcsum != 0 {
+		for i := 0; i < len(segments); i++ {
+			if segments[i].count != 0 || mc[i] != segments[i].modCount {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+/**
+ * Returns the number of key-value mappings in this map.  If the
+ * map contains more than <tt>Integer.MAX_VALUE</tt> elements, returns
+ * <tt>Integer.MAX_VALUE</tt>.
+ *
+ * @return the number of key-value mappings in this map
+ */
+func (this *ConcurrentMap) Size() int32 {
+	segments := this.segments
+	var sum int32 = 0
+	var check int32 = 0
+	mc := make([]int32, len(segments))
+	// Try a few times to get accurate count. On failure due to
+	// continuous async changes in table, resort to locking.
+	for k := 0; k < RETRIES_BEFORE_LOCK; k++ {
+		check = 0
+		sum = 0
+		var mcsum int32 = 0
+		for i := 0; i < len(segments); i++ {
+			sum += atomic.LoadInt32(&segments[i].count)
+			mc[i] = atomic.LoadInt32(&segments[i].modCount)
+			mcsum += mc[i]
+		}
+		if mcsum != 0 {
+			for i := 0; i < len(segments); i++ {
+				check += atomic.LoadInt32(&segments[i].count)
+				if mc[i] != atomic.LoadInt32(&segments[i].modCount) {
+					check = -1 // force retry
+					break
+				}
+			}
+		}
+		if check == sum {
+			break
+		}
+	}
+	if check != sum { // Resort to locking all segments
+		sum = 0
+		for i := 0; i < len(segments); i++ {
+			segments[i].lock.Lock()
+		}
+		for i := 0; i < len(segments); i++ {
+			sum += segments[i].count
+		}
+		for i := 0; i < len(segments); i++ {
+			segments[i].lock.Unlock()
+		}
+	}
+	return sum
+}
+
+/**
+ * Returns the value to which the specified key is mapped,
+ * or {@code nil} if this map contains no mapping for the key.
+ *
+ * <p>More formally, if this map contains a mapping from a key
+ * {@code k} to a value {@code v} such that {@code key.equals(k)},
+ * then this method returns {@code v}; otherwise it returns
+ * {@code nil}.  (There can be at most one such mapping.)
+ *
+ * @throws NullPointerException if the specified key is nil
+ */
+func (this *ConcurrentMap) Get(key interface{}) interface{} {
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).get(key, hash)
+}
+
+/**
+ * Tests if the specified object is a key in this table.
+ *
+ * @param  key   possible key
+ * @return <tt>true</tt> if and only if the specified object
+ *         is a key in this table, as determined by the
+ *         <tt>equals</tt> method; <tt>false</tt> otherwise.
+ * @throws NullPointerException if the specified key is nil
+ */
+func (this *ConcurrentMap) ContainsKey(key interface{}) bool {
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).containsKey(key, hash)
+}
+
+/**
+ * Maps the specified key to the specified value in this table.
+ * Neither the key nor the value can be nil.
+ *
+ * <p> The value can be retrieved by calling the <tt>get</tt> method
+ * with a key that is equal to the original key.
+ *
+ * @param key key with which the specified value is to be associated
+ * @param value value to be associated with the specified key
+ * @return the previous value associated with <tt>key</tt>, or
+ *         <tt>nil</tt> if there was no mapping for <tt>key</tt>
+ * @throws NullPointerException if the specified key or value is nil
+ */
+func (this *ConcurrentMap) Put(key interface{}, value interface{}) interface{} {
+	if value == nil {
+		panic(errors.New("NullPointerException"))
+	}
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).put(key, hash, value, false)
+}
+
+/**
+ * {@inheritDoc}
+ *
+ * @return the previous value associated with the specified key,
+ *         or <tt>nil</tt> if there was no mapping for the key
+ * @throws NullPointerException if the specified key or value is nil
+ */
+func (this *ConcurrentMap) PutIfAbsent(key interface{}, value interface{}) interface{} {
+	if value == nil {
+		panic(errors.New("NullPointerException"))
+	}
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).put(key, hash, value, false)
+}
+
+/**
+ * Copies all of the mappings from the specified map to this one.
+ * These mappings replace any mappings that this map had for any of the
+ * keys currently in the specified map.
+ *
+ * @param m mappings to be stored in this map
+ */
+func (this *ConcurrentMap) PutAll(m map[interface{}]interface{}) {
+	for k, v := range m {
+		this.Put(k, v)
+	}
+}
+
+/**
+ * Removes the key (and its corresponding value) from this map.
+ * This method does nothing if the key is not in the map.
+ *
+ * @param  key the key that needs to be removed
+ * @return the previous value associated with <tt>key</tt>, or
+ *         <tt>nil</tt> if there was no mapping for <tt>key</tt>
+ * @throws NullPointerException if the specified key is nil
+ */
+func (this *ConcurrentMap) Remove(key interface{}) interface{} {
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).remove(key, hash, nil)
+}
+
+/**
+ * {@inheritDoc}
+ *
+ * @throws NullPointerException if the specified key is nil
+ */
+func (this *ConcurrentMap) RemoveKV(key interface{}, value interface{}) bool {
+	hash := hash(hashVal(key))
+	if value == nil {
+		return false
+	}
+	return this.segmentFor(hash).remove(key, hash, value) != nil
+}
+
+/**
+ * {@inheritDoc}
+ *
+ * @throws NullPointerException if any of the arguments are nil
+ */
+func (this *ConcurrentMap) ReplaceWithOld(key interface{}, oldValue interface{}, newValue interface{}) interface{} {
+	if oldValue == nil || newValue == nil {
+		panic(errors.New("NullPointerException"))
+	}
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).replaceWithOld(key, hash, oldValue, newValue)
+}
+
+/**
+ * {@inheritDoc}
+ *
+ * @return the previous value associated with the specified key,
+ *         or <tt>nil</tt> if there was no mapping for the key
+ * @throws NullPointerException if the specified key or value is nil
+ */
+func (this *ConcurrentMap) Replace(key interface{}, value interface{}) interface{} {
+	if value == nil {
+		panic(errors.New("NullPointerException"))
+	}
+	hash := hash(hashVal(key))
+	return this.segmentFor(hash).replace(key, hash, value)
+}
+
+/**
+ * Removes all of the mappings from this map.
+ */
+func (this *ConcurrentMap) Clear() {
+	for i := 0; i < len(this.segments); i++ {
+		this.segments[i].clear()
+	}
+}
+
+func NewConcurrentMap2(initialCapacity int, loadFactor float32) (m *ConcurrentMap) {
+	return NewConcurrentMap3(initialCapacity, loadFactor, DEFAULT_CONCURRENCY_LEVEL)
+}
+
+/**
+ * Creates a new, empty map with the specified initial capacity,
+ * and with default load factor (0.75) and concurrencyLevel (16).
+ *
+ * @param initialCapacity the initial capacity. The implementation
+ * performs internal sizing to accommodate this many elements.
+ * @throws IllegalArgumentException if the initial capacity of
+ * elements is negative.
+ */
+func NewConcurrentMap1(initialCapacity int) (m *ConcurrentMap) {
+	return NewConcurrentMap3(initialCapacity, DEFAULT_LOAD_FACTOR, DEFAULT_CONCURRENCY_LEVEL)
+}
+
+/**
+ * Creates a new, empty map with a default initial capacity (16),
+ * load factor (0.75) and concurrencyLevel (16).
+ */
+func NewConcurrentMap() (m *ConcurrentMap) {
+	return NewConcurrentMap3(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR, DEFAULT_CONCURRENCY_LEVEL)
 }
 
 /**
@@ -78,11 +422,11 @@ func (this *ConcurrentMap) segmentFor(hash uint32) *Segment {
  * out as a user-visible Map.Entry.
  *
  * Because the value field is volatile, not final, it is legal wrt
- * the Java Memory Model for an unsynchronized reader to see null
+ * the Java Memory Model for an unsynchronized reader to see nil
  * instead of initial value when read via a data race.  Although a
  * reordering leading to this is not likely to ever actually
  * occur, the Segment.readValueUnderLock method is used as a
- * backup in case a null (pre-initialized) value is ever seen in
+ * backup in case a nil (pre-initialized) value is ever seen in
  * an unsynchronized access method.
  */
 type Entry struct {
@@ -96,7 +440,7 @@ type Segment struct {
 	/**
 	 * The number of elements in this segment's region.
 	 */
-	count int
+	count int32
 
 	/**
 	 * Number of updates that alter the size of the table. This is
@@ -106,14 +450,14 @@ type Segment struct {
 	 * we might have an inconsistent view of state so (usually)
 	 * must retry.
 	 */
-	modCount int
+	modCount int32
 
 	/**
 	 * The table is rehashed when its size exceeds this threshold.
 	 * (The value of this field is always <tt>(int)(capacity *
 	 * loadFactor)</tt>.)
 	 */
-	threshold int
+	threshold int32
 
 	/**
 	 * The per-segment table.
@@ -127,6 +471,255 @@ type Segment struct {
 	 * @serial
 	 */
 	loadFactor float32
+
+	lock *sync.Mutex
+}
+
+func (this *Segment) rehash() {
+	oldTable := this.table
+	oldCapacity := len(oldTable)
+	if oldCapacity >= MAXIMUM_CAPACITY {
+		return
+	}
+
+	/*
+	 * Reclassify nodes in each list to new Map.  Because we are
+	 * using power-of-two expansion, the elements from each bin
+	 * must either stay at same index, or move with a power of two
+	 * offset. We eliminate unnecessary node creation by catching
+	 * cases where old nodes can be reused because their next
+	 * fields won't change. Statistically, at the default
+	 * threshold, only about one-sixth of them need cloning when
+	 * a table doubles. The nodes they replace will be garbage
+	 * collectable as soon as they are no longer referenced by any
+	 * reader thread that may be in the midst of traversing table
+	 * right now.
+	 */
+
+	newTable := make([]*Entry, oldCapacity<<1)
+	this.threshold = int32(float32(len(newTable)) * this.loadFactor)
+	sizeMask := uint32(len(newTable) - 1)
+	for i := 0; i < oldCapacity; i++ {
+		// We need to guarantee that any existing reads of old Map can
+		//  proceed. So we cannot yet nil out each bin.
+		e := oldTable[i]
+
+		if e != nil {
+			next := e.next
+			//计算扩容后新的数组下标
+			idx := e.hash & sizeMask
+
+			//  Single node on list
+			//如果没有后续的碰撞节点，直接复制到新数组即可
+			if next == nil {
+				newTable[idx] = e
+			} else {
+				// Reuse trailing consecutive sequence at same slot
+				// 数组扩容后原来下标相同（碰撞）的节点可能下标会出现不同
+				// 下面的代码为了提高效率，会循环碰撞链表，找到链表中最后出现的新下标相同节点的首节点
+				// 然后将这个首节点复制到新数组，后续节点因为计算出的新下标相同，所以在扩容后的数组中仍然是碰撞
+				// 所以新的首节点的碰撞链表是正确的
+				// 新的首节点之外的其他现存碰撞链表上的节点，则重新复制到新节点（这个重要）后放入新数组
+				// 这个过程的关键在于维持所有旧节点的next属性不会发生变化，这样才能让无锁的读操作保持线程安全
+				lastRun := e
+				lastIdx := idx
+				for last := next; last != nil; last = last.next {
+					k := last.hash & uint32(sizeMask)
+					//发现新下标不同的节点就保存到lastIdx和lastRun中
+					//所以lastIdx和lastRun总是对应现有碰撞链表中最后一个新下标和其对应节点
+					if k != lastIdx {
+						lastIdx = k
+						lastRun = last
+					}
+				}
+				newTable[lastIdx] = lastRun
+
+				// Clone all remaining nodes
+				for p := e; p != lastRun; p = p.next {
+					k := p.hash & sizeMask
+					n := newTable[k]
+					newTable[k] = &Entry{p.key, p.hash, p.value, n}
+				}
+			}
+		}
+	}
+	this.table = newTable
+}
+
+/**
+ * Sets table to new HashEntry array.
+ * Call only while holding lock or in constructor.
+ */
+func (this *Segment) setTable(newTable []*Entry) {
+	this.threshold = (int32)(float32(len(newTable)) * this.loadFactor)
+	this.table = newTable
+}
+
+/**
+ * Returns properly casted first entry of bin for given hash.
+ */
+func (this *Segment) getFirst(hash uint32) *Entry {
+	tab := this.table
+	return tab[hash&uint32(len(tab)-1)]
+}
+
+/**
+ * Reads value field of an entry under lock. Called if value
+ * field ever appears to be nil. This is possible only if a
+ * compiler happens to reorder a HashEntry initialization with
+ * its table assignment, which is legal under memory model
+ * but is not known to ever occur.
+ */
+func (this *Segment) readValueUnderLock(e *Entry) interface{} {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	return e.value
+}
+
+/* Specialized implementations of map methods */
+
+func (this *Segment) get(key interface{}, hash uint32) interface{} {
+	if atomic.LoadInt32(&this.count) != 0 { // read-volatile
+		e := this.getFirst(hash)
+		for e != nil {
+			if e.hash == hash && key == e.key {
+				v := e.value
+				if v != nil {
+					return v
+				}
+				return this.readValueUnderLock(e) // recheck
+			}
+			e = e.next
+		}
+	}
+	return nil
+}
+
+func (this *Segment) containsKey(key interface{}, hash uint32) bool {
+	if atomic.LoadInt32(&this.count) != 0 { // read-volatile
+		e := this.getFirst(hash)
+		for e != nil {
+			if e.hash == hash && key == e.key {
+				return true
+			}
+			e = e.next
+		}
+	}
+	return false
+}
+
+func (this *Segment) replaceWithOld(key interface{}, hash uint32, oldValue interface{}, newValue interface{}) bool {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	e := this.getFirst(hash)
+	for e != nil && (e.hash != hash || key != e.key) {
+		e = e.next
+	}
+
+	replaced := false
+	if e != nil && oldValue == e.value {
+		replaced = true
+		e.value = newValue
+	}
+	return replaced
+}
+
+func (this *Segment) replace(key interface{}, hash uint32, newValue interface{}) (oldValue interface{}) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	e := this.getFirst(hash)
+	for e != nil && (e.hash != hash || key != e.key) {
+		e = e.next
+	}
+
+	if e != nil {
+		oldValue = e.value
+		e.value = newValue
+	}
+	return
+}
+
+func (this *Segment) put(key interface{}, hash uint32, value interface{}, onlyIfAbsent bool) (oldValue interface{}) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	c := this.count
+	if c > this.threshold { // ensure capacity
+		this.rehash()
+	}
+	c++
+
+	tab := this.table
+	index := hash & uint32(len(tab)-1)
+	first := tab[index]
+	e := first
+	for e != nil && (e.hash != hash || key != e.key) {
+		e = e.next
+	}
+
+	if e != nil {
+		oldValue = e.value
+		if !onlyIfAbsent {
+			e.value = value
+		}
+	} else {
+		oldValue = nil
+		this.modCount++
+		tab[index] = &Entry{key, hash, value, first}
+		this.count = c // write-volatile
+	}
+	return
+}
+
+/**
+ * Remove; match on key only if value nil, else match both.
+ */
+func (this *Segment) remove(key interface{}, hash uint32, value interface{}) (oldValue interface{}) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	c := this.count - 1
+	tab := this.table
+	index := hash & uint32(len(tab)-1)
+	first := tab[index]
+	e := first
+
+	for e != nil && (e.hash != hash || key != e.key) {
+		e = e.next
+	}
+
+	if e != nil {
+		v := e.value
+		if value == nil || value == v {
+			oldValue = v
+			// All entries following removed node can stay
+			// in list, but all preceding ones need to be
+			// cloned.
+			this.modCount++
+			newFirst := e.next
+			for p := first; p != e; p = p.next {
+				newFirst = &Entry{p.key, p.hash, p.value, newFirst}
+			}
+			tab[index] = newFirst
+			this.count = c
+		}
+	}
+	return
+}
+
+func (this *Segment) clear() {
+	if atomic.LoadInt32(&this.count) != 0 {
+		this.lock.Lock()
+		defer this.lock.Unlock()
+
+		tab := this.table
+		for i := 0; i < len(tab); i++ {
+			tab[i] = nil
+		}
+		this.modCount++
+		this.count = 0 // write-volatile
+	}
 }
 
 func newSegment(initialCapacity int, lf float32) (s *Segment) {
@@ -152,4 +745,8 @@ func hash(h uint32) uint32 {
 	h ^= (h >> 6)
 	h += (h << 2) + (h << 14)
 	return h ^ (h >> 16)
+}
+
+func hashVal(v interface{}) uint32 {
+	panic(errors.New("outstanding item!"))
 }
